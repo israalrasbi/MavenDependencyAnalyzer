fbfcbfc9b2f4ebcdfb2339b4c37168ab11720d91
isra <imorasbi@gmail.com>
2025-02-05 14:30:39 +0400

    Added the Maven Dependency Analyzer files

---
+++ src/main/java/com/isra/security/dependency_analyzer/DependencyAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.isra.security.dependency_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyAnalyzerApplication.class, args);
+	}
+
+}
