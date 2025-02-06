50c532ca3211cf9df8ffdfcdeaa0c0fe672de90c
isra <imorasbi@gmail.com>
2025-02-06 12:16:01 +0400

    Implemented the DependencyService

--- src/main/java/com/isra/security/dependency_analyzer/Services/DependencyService.java
+++ src/main/java/com/isra/security/dependency_analyzer/Services/DependencyService.java
@@ -1,4 +1,17 @@
 package com.isra.security.dependency_analyzer.Services;
 
+import com.isra.security.dependency_analyzer.DTOs.SBOMRequest;
+import com.isra.security.dependency_analyzer.Interfaces.SBOMGeneratorInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DependencyService {
+    @Autowired
+    SBOMGeneratorInterface sbomGeneratorInterface;
+
+    public String generateBomJson(SBOMRequest sbomRequest){
+        return sbomGeneratorInterface.generateSBOMFromPOM(sbomRequest.getBomJson());
+    }
+
 }
