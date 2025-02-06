dd9a999fd7c88822a9c1dacfc30303c332f91175
isra <imorasbi@gmail.com>
2025-02-06 10:44:39 +0400

    Implemented the SBOMRequest class
    - Added variables and getters and setters functions.

--- src/main/java/com/isra/security/dependency_analyzer/DTOs/SBOMRequest.java
+++ src/main/java/com/isra/security/dependency_analyzer/DTOs/SBOMRequest.java
@@ -1,4 +1,22 @@
 package com.isra.security.dependency_analyzer.DTOs;
 
 public class SBOMRequest {
+    private String projectName;
+    private String bomJson;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getBomJson() {
+        return bomJson;
+    }
+
+    public void setBomJson(String bomJson) {
+        this.bomJson = bomJson;
+    }
 }
