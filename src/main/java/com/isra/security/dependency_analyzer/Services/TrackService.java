ba027c16f5b57216e81e74c1e3bc199d4dc50fec
isra <imorasbi@gmail.com>
2025-02-06 12:24:06 +0400

    Implemented the TrackService

--- src/main/java/com/isra/security/dependency_analyzer/Services/TrackService.java
+++ src/main/java/com/isra/security/dependency_analyzer/Services/TrackService.java
@@ -1,4 +1,22 @@
 package com.isra.security.dependency_analyzer.Services;
 
+import com.isra.security.dependency_analyzer.DTOs.VulnerabilityResponse;
+import com.isra.security.dependency_analyzer.Interfaces.DependencyTrackClientInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TrackService {
+    @Autowired
+    DependencyTrackClientInterface dependencyTrackClientInterface;
+
+    public List<VulnerabilityResponse> analyzeBOM(String bomJson){
+        return dependencyTrackClientInterface.analyze(bomJson);
+    }
+
+    public List<VulnerabilityResponse> getVulnerabilities(String projectName){
+        return dependencyTrackClientInterface.getVulnerabilitiesByProjectName(projectName);
+    }
 }
