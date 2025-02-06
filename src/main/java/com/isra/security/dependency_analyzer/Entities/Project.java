db216b4ffd8c2b619f799b1da177bfc9a833f790
isra <imorasbi@gmail.com>
2025-02-06 10:22:26 +0400

    Implemented the Project class
    - Added variables and getters and setters functions.

--- src/main/java/com/isra/security/dependency_analyzer/Entities/Project.java
+++ src/main/java/com/isra/security/dependency_analyzer/Entities/Project.java
@@ -1,6 +1,34 @@
 package com.isra.security.dependency_analyzer.Entities;
 
 
+import java.util.List;
+
 public class Project {
     private Integer id;
+    private String projectName;
+    private List<Dependency> dependencyList;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public List<Dependency> getDependencyList() {
+        return dependencyList;
+    }
+
+    public void setDependencyList(List<Dependency> dependencyList) {
+        this.dependencyList = dependencyList;
+    }
 }
