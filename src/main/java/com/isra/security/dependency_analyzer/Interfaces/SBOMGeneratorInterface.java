822c7066a6ec121b23899d7c6eaf57282b5d045a
isra <imorasbi@gmail.com>
2025-02-06 12:06:10 +0400

    Implemented the SBOMGeneratorInterface

--- src/main/java/com/isra/security/dependency_analyzer/Interfaces/SBOMGeneratorInterface.java
+++ src/main/java/com/isra/security/dependency_analyzer/Interfaces/SBOMGeneratorInterface.java
@@ -1,4 +1,6 @@
 package com.isra.security.dependency_analyzer.Interfaces;
 
 public interface SBOMGeneratorInterface {
+    //method to generate bom.json from pom.xml
+    String generateSBOMFromPOM(String pomXml);
 }
