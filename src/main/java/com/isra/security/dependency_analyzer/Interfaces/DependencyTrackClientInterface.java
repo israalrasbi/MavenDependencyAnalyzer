c9394f5475fd3a280e4b48d2252c170329f42b1b
isra <imorasbi@gmail.com>
2025-02-06 12:03:28 +0400

    Implemented the DependencyTrackClientInterface

--- src/main/java/com/isra/security/dependency_analyzer/Interfaces/DependencyTrackClientInterface.java
+++ src/main/java/com/isra/security/dependency_analyzer/Interfaces/DependencyTrackClientInterface.java
@@ -1,4 +1,13 @@
 package com.isra.security.dependency_analyzer.Interfaces;
 
+import com.isra.security.dependency_analyzer.DTOs.VulnerabilityResponse;
+
+import java.util.List;
+
 public interface DependencyTrackClientInterface {
+    //method to send the bom.json to Dependency-Track and receive results
+    List<VulnerabilityResponse> analyze(String bomJson);
+
+    //method to get the vulnerability list by project name
+    List<VulnerabilityResponse> getVulnerabilitiesByProjectName(String projectName);
 }
