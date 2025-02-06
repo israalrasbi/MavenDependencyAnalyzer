9cecbc2076cdfe1528bf3d9cea862f5d23889040
isra <imorasbi@gmail.com>
2025-02-06 11:07:32 +0400

    Updated the SBOMRequest
    - Added variable id, with it's setter and getter methods
     - Added methods to convert from and to DTO
    - Added Utils package that contains HelperUtils

--- src/main/java/com/isra/security/dependency_analyzer/DTOs/SBOMRequest.java
+++ src/main/java/com/isra/security/dependency_analyzer/DTOs/SBOMRequest.java
@@ -1,8 +1,22 @@
 package com.isra.security.dependency_analyzer.DTOs;
 
+import com.isra.security.dependency_analyzer.Entities.Project;
+import com.isra.security.dependency_analyzer.Utils.HelperUtils;
+
+import java.util.ArrayList;
+
 public class SBOMRequest {
+    private Integer id;
     private String projectName;
     private String bomJson;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getProjectName() {
         return projectName;
@@ -18,5 +32,25 @@ public class SBOMRequest {
 
     public void setBomJson(String bomJson) {
         this.bomJson = bomJson;
+    }
+
+    public static SBOMRequest convertToDTO(Project project, String bomJsonContent){
+        SBOMRequest dto = new SBOMRequest();
+        if(HelperUtils.isNotNull(project)){
+            dto.setId(project.getId());
+            dto.setProjectName(project.getProjectName());
+            dto.setBomJson(bomJsonContent);
+        }
+        return dto;
+    }
+
+    public static Project convertToEntity(SBOMRequest dto){
+        Project entity = new Project();
+        if(HelperUtils.isNotNull(dto)){
+            entity.setId(dto.getId());
+            entity.setProjectName(dto.getProjectName());
+            entity.setDependencyList(new ArrayList<>());
+        }
+        return entity;
     }
 }
