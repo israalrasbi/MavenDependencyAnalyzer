5f826f837571048c709409eefb990d7a2f2d218e
isra <imorasbi@gmail.com>
2025-02-11 23:06:17 +0400

    Added variables to AIService class

--- src/main/java/com/isra/security/dependency_analyzer/Services/AIService.java
+++ src/main/java/com/isra/security/dependency_analyzer/Services/AIService.java
@@ -1,5 +1,14 @@
 package com.isra.security.dependency_analyzer.Services;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AIService {
+    @Autowired
+    private ChatClient chatClient;
+
+    private static final String FILE_PATH = "output/vulnerabilities.json";
 
 }
