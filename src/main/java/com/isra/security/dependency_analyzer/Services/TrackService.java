2c791abc86409e309a1ca784a8f3c02bffe539ca
isra <imorasbi@gmail.com>
2025-02-11 12:25:16 +0400

    Added variables to TrackService

--- src/main/java/com/isra/security/dependency_analyzer/Services/TrackService.java
+++ src/main/java/com/isra/security/dependency_analyzer/Services/TrackService.java
@@ -3,11 +3,19 @@ package com.isra.security.dependency_analyzer.Services;
 import com.isra.security.dependency_analyzer.DTOs.VulnerabilityResponse;
 import com.isra.security.dependency_analyzer.Interfaces.DependencyTrackClientInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 public class TrackService {
-   
+    @Value("${dependency.track.api.url}")
+    private String dependencyTrackApiUrl;
+
+    @Value("${dependency.track.api.key}")
+    private String apiKey;
+
+    private RestTemplate restTemplate;
 }
