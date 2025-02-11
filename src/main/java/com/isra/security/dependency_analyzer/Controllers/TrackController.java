4ec949befa29f06d9461c895cfb7361eabac6fa8
isra <imorasbi@gmail.com>
2025-02-11 12:46:41 +0400

    Added uploadSbom function in TrackController

--- src/main/java/com/isra/security/dependency_analyzer/Controllers/TrackController.java
+++ src/main/java/com/isra/security/dependency_analyzer/Controllers/TrackController.java
@@ -2,12 +2,17 @@ package com.isra.security.dependency_analyzer.Controllers;
 
 import com.isra.security.dependency_analyzer.Services.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/track")
 public class TrackController {
     @Autowired
     private TrackService trackService;
+    
+    @PostMapping("/upload-sbom")
+    public ResponseEntity<String> uploadSbom(@RequestParam String projectUuid, @RequestBody String bomJson) {
+        return trackService.uploadSbom(projectUuid, bomJson);
+    }
 }
