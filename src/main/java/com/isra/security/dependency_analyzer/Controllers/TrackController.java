1e09b90d672be9733ef4fdddd1c3b242d9307536
isra <imorasbi@gmail.com>
2025-02-11 13:50:03 +0400

    Added a GET api in TrackController

--- src/main/java/com/isra/security/dependency_analyzer/Controllers/TrackController.java
+++ src/main/java/com/isra/security/dependency_analyzer/Controllers/TrackController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/track")
 public class TrackController {
@@ -14,5 +17,10 @@ public class TrackController {
     @PostMapping("/uploadSbom")
     public ResponseEntity<String> uploadSbom(@RequestParam String projectUuid, @RequestParam String filePath) {
         return trackService.uploadSbomFromFile(projectUuid, filePath);
+    }
+
+    @GetMapping("/getVulnerabilities")
+    public ResponseEntity<List<Map<String, Object>>> getVulnerabilities(@RequestParam String projectUuid) {
+        return ResponseEntity.ok(trackService.getVulnerabilities(projectUuid));
     }
 }
