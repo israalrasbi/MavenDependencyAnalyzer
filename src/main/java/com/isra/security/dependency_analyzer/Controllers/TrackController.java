7cc6a83ceafdcf6129b2c618cf346a6dda3ba5e4
isra <imorasbi@gmail.com>
2025-02-11 12:45:30 +0400

    Called TrackService in TrackController

--- src/main/java/com/isra/security/dependency_analyzer/Controllers/TrackController.java
+++ src/main/java/com/isra/security/dependency_analyzer/Controllers/TrackController.java
@@ -1,7 +1,13 @@
 package com.isra.security.dependency_analyzer.Controllers;
 
+import com.isra.security.dependency_analyzer.Services.TrackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/track")
 public class TrackController {
+    @Autowired
+    private TrackService trackService;
 }
