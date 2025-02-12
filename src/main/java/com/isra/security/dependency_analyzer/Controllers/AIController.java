293876d362d659f9ec351d287f65eb92015f70f6
isra <imorasbi@gmail.com>
2025-02-12 09:00:50 +0400

    Added variable to AIService

--- src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
+++ src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
@@ -1,9 +1,13 @@
 package com.isra.security.dependency_analyzer.Controllers;
 
+import com.isra.security.dependency_analyzer.Services.AIService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "ai")
 public class AIController {
+    @Autowired
+    private AIService aiService;
 }
