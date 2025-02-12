0e8f30104ab77fd037e2261590318ae2ad132441
isra <imorasbi@gmail.com>
2025-02-12 09:01:29 +0400

    Implemented A get method in AIController

--- src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
+++ src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
@@ -2,6 +2,7 @@ package com.isra.security.dependency_analyzer.Controllers;
 
 import com.isra.security.dependency_analyzer.Services.AIService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AIController {
     @Autowired
     private AIService aiService;
+
+    @GetMapping("/analyze")
+    public String analyzeVulnerabilities() {
+        return aiService.analyzeVulnerabilities();
+    }
 }
