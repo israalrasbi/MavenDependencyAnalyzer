6f142184581db168d09eb373e52f872051db566d
isra <imorasbi@gmail.com>
2025-02-12 09:00:11 +0400

    Added annotations in AIController class

--- src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
+++ src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
@@ -1,4 +1,9 @@
 package com.isra.security.dependency_analyzer.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "ai")
 public class AIController {
 }
