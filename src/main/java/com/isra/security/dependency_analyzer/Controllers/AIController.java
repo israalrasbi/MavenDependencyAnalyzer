6b6c901c2917de4d6c29f5e338cd4026f9cab300
isra <imorasbi@gmail.com>
2025-02-13 09:44:51 +0400

    Added functions to handle API in AIController

--- src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
+++ src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
@@ -3,22 +3,22 @@ package com.isra.security.dependency_analyzer.Controllers;
 import com.isra.security.dependency_analyzer.Services.AIService;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping(value = "ai")
 public class AIController {
 
+    /*
     @Autowired
     private AIService aiService;
-/*
+
     @GetMapping("/analyze")
     public String analyzeVulnerabilities() {
         return aiService.analyzeVulnerabilities();
-    }*/
+    }
 
     private final ChatClient chatClient;
 
@@ -29,5 +29,35 @@ public class AIController {
     @GetMapping("/analyze")
     public String analyze(@RequestParam(value = "message", defaultValue = "Tell me the best programming language") String message) {
         return aiService.runDeepseekR1(message);
+    }*/
+
+    private ChatClient chatClient;
+
+    public AIController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
     }
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> promptWithPathVariable(@PathVariable String chat) {
+        try {
+            String response = chatClient
+                    .prompt(chat)
+                    .call()
+                    .content();
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/stream/chat")
+    public Flux<String> streamChat(@PathVariable String chat) {
+        return chatClient
+                .prompt()
+                .user(chat)
+                .stream()
+                .content();
+    }
+
+
 }
