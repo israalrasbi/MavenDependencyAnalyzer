7a0586fbaa7d880a61ca492cfd4e6d99596b298c
Isra Al Rasbi <imorasbi@gmail.com>
2025-02-16 08:55:14 +0400

    Removed unneeded libraries

--- src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
+++ src/main/java/com/isra/security/dependency_analyzer/Controllers/AIController.java
@@ -1,16 +1,14 @@
 package com.isra.security.dependency_analyzer.Controllers;
 
 import com.isra.security.dependency_analyzer.Services.AIService;
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @RestController
 @RequestMapping(value = "ai")
