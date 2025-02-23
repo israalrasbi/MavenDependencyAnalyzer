4fadbe94e85c84c4d5843cdd0ac14c58f83a2054
Isra Al Rasbi <imorasbi@gmail.com>
2025-02-23 08:51:51 +0400

    Added variables to User class

--- src/main/java/com/isra/security/dependency_analyzer/Entities/User.java
+++ src/main/java/com/isra/security/dependency_analyzer/Entities/User.java
@@ -1,4 +1,15 @@
 package com.isra.security.dependency_analyzer.Entities;
 
+import javax.management.relation.Role;
+import java.util.Date;
+import java.util.UUID;
+
 public class User {
+    private boolean isActive;
+    private Date createdDate;
+    private Date updatedDate;
+    private UUID id;
+    private String username;
+    private String password;
+    private Role role;
 }
