d3097fe8a46e9d1b1d4f77e7d83b326792644b48
Isra Al Rasbi <imorasbi@gmail.com>
2025-02-24 08:56:31 +0400

    Create SignInRequestDTO.java

---
+++ src/main/java/com/isra/security/dependency_analyzer/DTOs/SignInRequestDTO.java
@@ -0,0 +1,22 @@
+package com.isra.security.dependency_analyzer.DTOs;
+
+public class SignInRequestDTO {
+    String userName;
+    String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
