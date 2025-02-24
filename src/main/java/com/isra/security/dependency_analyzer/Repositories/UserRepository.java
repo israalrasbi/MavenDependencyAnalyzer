0ad4cd15baad57f01f65998bfc3c5d6f19011371
Isra Al Rasbi <imorasbi@gmail.com>
2025-02-24 14:41:50 +0400

    Created UserRepository  interface

---
+++ src/main/java/com/isra/security/dependency_analyzer/Repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.isra.security.dependency_analyzer.Repositories;
+
+import com.isra.security.dependency_analyzer.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.userName =:un")
+    User findByUserName(@Param("un") String userName);
+
+    Boolean existsByEmail(String email);
+}
